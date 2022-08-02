2258687613263befe28bb9ac886e0be1c6f3ae15
Quinn Dougherty <quinnd@riseup.net>
2022-08-01 21:04:32 -0400

    actual coding error for once

--- ci.nix
+++ ci.nix
@@ -3,11 +3,11 @@ builtins.mapAttrs (k: _v:
   let
     url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
     pkgs = import (builtins.fetchTarball url) { system = k; };
+    squiggleMonorepo = import ./default.nix { inherit pkgs; }; 
   in
   pkgs.recurseIntoAttrs {
-    # These two attributes will appear in your job for each platform.
-    squiggleMonorepo = import ./default.nix { inherit pkgs; };
     sources = squiggleMonorepo.sources;  
+    nodeDependencies = squiggleMonorepo.nodeDependencies;
   }
 ) {
   x86_64-linux = {};
