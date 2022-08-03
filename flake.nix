68b59a93cb6d7fd07af0fe732de09851b4857d3e
Quinn Dougherty <quinnd@riseup.net>
2022-08-03 15:12:26 -0400

    current best guesses

--- flake.nix
+++ flake.nix
@@ -4,41 +4,35 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, hercules-ci-effects, ... }:
     let
+      system = "x86_64-linux";
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [  ];
+        system = system;
+        overlays = [ hercules-ci-effects.overlay ];
       };
 
       # set the node version here
       nodejs = pkgs.nodejs-16_x;
       commonBuildInputs = [ nodejs pkgs.yarn ];
+      commonYarnFlags = [
+                "--offline"
+                "--frozen-lockfile"
+                "--verbose"
+                "--production=false"
+      ];
 
-      squiggle-lang-yarnPackage = pkgs.mkYarnPackage rec {
+      squiggle-lang-yarnPackage = pkgs.mkYarnPackage {
               name = "squiggle-lang_source";
               buildInputs = commonBuildInputs;
+              # extraNativeBuildInputs = [ pkgs.which ];
               src = ./packages/squiggle-lang;
               packageJSON = ./packages/squiggle-lang/package.json;
               yarnLock = ./yarn.lock;
-
-              # for testing
-              yarnFlags = [
-                "--offline"
-                "--frozen-lockfile"
-                "--ignore-engines"
-                "--verbose"
-              ];
-              patchPhase = ''
-                mkdir -p tmpGlobal
-                echo "--pack.offline true" >> .yarnrc
-                echo "--lint.offline true" >> .yarnrc
-                echo "--pack.frozen-lockfile true" >> .yarnrc
-                echo "--pack.verbose true" >> .yarnrc
-                echo "--build:rescript.global-folder tmpGlobal" >> .yarnrc
-              '';
+              yarnFlags = commonYarnFlags;
       };
       squiggle-lang-lint = pkgs.stdenv.mkDerivation {
         name = "squiggle-lang-lint";
@@ -52,33 +46,37 @@
       squiggle-lang = pkgs.stdenv.mkDerivation {
             name = "squiggle-lang";
             src = squiggle-lang-yarnPackage + "/libexec/@quri/squiggle-lang/";
-            buildInputs = commonBuildInputs;
-            buildPhase = "cd deps/@quri/squiggle-lang && yarn pack";
+            buildInputs = commonBuildInputs; # ++ [ pkgs.patchelf pkgs.which ];
+            # `peggy` is in the `node_modules` that's adjacent to `deps`.
+            buildPhase = ''
+              cd deps/@quri/squiggle-lang
+              mkdir -p homeless_shelter/.yarn
+              yarn pack
+            '';
             installPhase = ''
               mkdir -p $out
               cp -r $src/libexec/@quri/squiggle-lang/deps/@quri/squiggle-lang/dist $out/dist
               cp -r $src/libexec/@quri/squiggle-lang/node_modules/ $out/node_modules
             '';
+            # passthru to spoof that this is still a yarn package even tho it's a subsequent derivation
+            workspaceDependencies = [];
+            pname = "@quri/squiggle-lang";
+            packageJSON = ./package.json;
       };
 
-      squiggle-components-yarnPackage = pkgs.mkYarnPackage rec {
+      squiggle-components-yarnPackage = pkgs.mkYarnPackage {
         name = "squiggle-components_source";
         buildInputs = commonBuildInputs;
-        src = ./.;
+        src = ./packages/components;
         packageJSON = ./packages/components/package.json;
         yarnLock = ./yarn.lock;
+        workspaceDependencies = [ squiggle-lang ];
+        yarnPreBuild = ''
+          mkdir -p $src/node_modules/@quri/squiggle-lang
+          cp -r ${squiggle-lang}/dist $src/node_modules/@quri/squiggle-lang
+        '';
 
-        # this runs after the packages are installed
-        pkgConfig.postInstall = "yarn all";
-
-        # for testing
-        yarnFlags = [
-          "--offline"
-          "--frozen-lockfile"
-          "--ignore-engines"
-          "--ignore-scripts"
-          "--verbose"
-        ];
+        yarnFlags = commonYarnFlags;
       };
       squiggle-components-lint = pkgs.stdenv.mkDerivation {
         name = "squiggle-components-lint";
@@ -88,64 +86,74 @@
       };
       squiggle-components = pkgs.stdenv.mkDerivation {
             name = "squiggle-components";
-            src = squiggle-components-yarnPackage + "/libexec/@quri/squiggle-components/deps/@quri/squiggle-components";
-            buildPhase = "yarn all";
+            src = squiggle-components-yarnPackage + "/libexec/@quri/squiggle-components/"; # deps/@quri/squiggle-components";
+            buildInputs = commonBuildInputs;
+            buildPhase = ''
+              cd deps/@quri/squiggle-components
+              yarn all
+            '';
             installPhase = ''
               mkdir -p $out
-              cp -R $src/libexec/squiggle-components/deps/squiggle-components/. $out
+              cp -R $src/libexec/squiggle-components/deps/squiggle-components $out
               rm $out/bin/node_modules
               cp -R $src/libexec/squiggle-components/node_modules/. $out/node_modules
             '';
           };
 
+      squiggle-website-yarnPackage = pkgs.mkYarnPackage {
+              name = "squiggle-website_source";
+              src = ./packages/website;
+              packageJSON = ./packages/website/package.json;
+              yarnLock = ./yarn.lock;
+              workspaceDependencies = [ squiggle-components-yarnPackage ];
+
+              # for testing
+              yarnFlags = commonYarnFlags;
+      };
+      squiggle-website-lint = pkgs.stdenv.mkDerivation {
+        name = "squiggle-website-lint";
+        buildInputs = commonBuildInputs;
+        src = squiggle-website-yarnPackage + "/libexec/squiggle-website/deps/squiggle-website";
+        buildPhase = "yarn lint";
+        installPhase = "mkdir -p $out";
+      };
+      squiggle-website = pkgs.stdenv.mkDerivation {
+        name = "squiggle-website";
+        buildInputs = commonBuildInputs;
+        src = squiggle-website-yarnPackage + "/libexec/squiggle-website/deps/squiggle-website";
+        buildPhase = "yarn build";
+        installPhase = ''
+              mkdir -p $out
+              cp -R $ src/libexec/squiggle-website/deps/squiggle-website/build $out
+              rm $out/bin/node_modules
+              cp -R $src/libexec/squiggle-website/node_modules/. $out/node_modules
+        '';
+      };
     in rec {
 
-      herculesCI = {
-        onPush = {
+
+      herculesCI.onPush = {
           squiggle-lang.outputs = {
             squiggle-lang = squiggle-lang;
             squiggle-lang-lint = squiggle-lang-lint;
           };
-          squiggle-components-lint.outputs = {
-            squiggle-components = squiggle-components;
-            squiggle-components-lint = squiggle-components-lint;
+          squiggle-components = {
+            outputs = {
+              squiggle-components = squiggle-components;
+              squiggle-components-lint = squiggle-components-lint;
+            };
+            extraInputs.lang-package = "";
           };
 
-          squiggle-website.outputs.squiggle-website = pkgs.recurseIntoAttrs (let
-            # base mkYarnPackage config
-            website = pkgs.mkYarnPackage {
-              name = "squiggle-website";
-              buildInputs = [
-                nodejs
-              ];
-              src = ./.;
-              packageJSON = ./packages/website/package.json;
-              yarnLock = ./yarn.lock;
-
-              # this runs after the packages are installed
-              pkgConfig.postInstall = "yarn build";
-
-              # for testing
-              yarnFlags = [
-                "--offline"
-                "--frozen-lockfile"
-                "--ignore-engines"
-                "--ignore-scripts"
-                "--verbose"
-              ];
+          squiggle-website = {
+            outputs = {
+              squiggle-website = squiggle-website;
+              squiggle-website-lint = squiggle-website-lint;
             };
-            # mkYarnPackage puts teh compelted files in a really nestled directory
-          in pkgs.stdenv.mkDerivation {
-            name = "squiggle-website";
-            src = website;
-            installPhase = ''
-              mkdir -p $out
-              cp -R $ src/libexec/squiggle-website/deps/squiggle-website/. $out
-              rm $out/bin/node_modules
-              cp -R $src/libexec/squiggle-website/node_modules/. $out/node_modules
-            '';
-          });
-        };
+            extraInputs.components-package = "";
+          };
       };
-    };
+      defaultPackage."${system}" = herculesCI.onPush.squiggle-components.outputs.squiggle-components-lint;
+
+      };
 }
