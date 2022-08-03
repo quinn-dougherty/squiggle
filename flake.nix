a3a75f5efa46acbd62150a6eb01182ac3eadadd0
Quinn Dougherty <quinnd@riseup.net>
2022-08-03 16:17:52 -0400

    I want this to work _so bad_ lol

--- flake.nix
+++ flake.nix
@@ -19,24 +19,24 @@
       nodejs = pkgs.nodejs-16_x;
       commonBuildInputs = [ nodejs pkgs.yarn ];
       commonYarnFlags = [
-                "--offline"
-                "--frozen-lockfile"
-                "--verbose"
-                "--production=false"
+        "--offline"
+        "--frozen-lockfile"
+        "--verbose"
+        "--production=false"
       ];
 
       squiggle-lang-yarnPackage = pkgs.mkYarnPackage {
-              name = "squiggle-lang_source";
-              buildInputs = commonBuildInputs;
-              # extraNativeBuildInputs = [ pkgs.which ];
-              src = ./packages/squiggle-lang;
-              packageJSON = ./packages/squiggle-lang/package.json;
-              yarnLock = ./yarn.lock;
-              yarnFlags = commonYarnFlags;
+        name = "squiggle-lang_source";
+        # extraNativeBuildInputs = [ pkgs.which ];
+        src = ./packages/squiggle-lang;
+        packageJSON = ./packages/squiggle-lang/package.json;
+        yarnLock = ./yarn.lock;
+        yarnFlags = commonYarnFlags;
       };
       squiggle-lang-lint = pkgs.stdenv.mkDerivation {
         name = "squiggle-lang-lint";
-        src = squiggle-lang-yarnPackage + "/libexec/@quri/squiggle-lang/deps/@quri/squiggle-lang";
+        src = squiggle-lang-yarnPackage
+          + "/libexec/@quri/squiggle-lang/deps/@quri/squiggle-lang";
         buildInputs = commonBuildInputs;
         # Can only do prettier because `.lint.sh` script for rescript doesn't work,
         # see https://stackoverflow.com/questions/18018359/all-newlines-are-removed-when-saving-cat-output-into-a-variable
@@ -44,24 +44,22 @@
         installPhase = "mkdir -p $out";
       };
       squiggle-lang = pkgs.stdenv.mkDerivation {
-            name = "squiggle-lang";
-            src = squiggle-lang-yarnPackage + "/libexec/@quri/squiggle-lang/";
-            buildInputs = commonBuildInputs; # ++ [ pkgs.patchelf pkgs.which ];
-            # `peggy` is in the `node_modules` that's adjacent to `deps`.
-            buildPhase = ''
-              cd deps/@quri/squiggle-lang
-              mkdir -p homeless_shelter/.yarn
-              yarn pack
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp -r $src/libexec/@quri/squiggle-lang/deps/@quri/squiggle-lang/dist $out/dist
-              cp -r $src/libexec/@quri/squiggle-lang/node_modules/ $out/node_modules
-            '';
-            # passthru to spoof that this is still a yarn package even tho it's a subsequent derivation
-            workspaceDependencies = [];
-            pname = "@quri/squiggle-lang";
-            packageJSON = ./package.json;
+        name = "squiggle-lang";
+        # `peggy` is in the `node_modules` that's adjacent to `deps`.
+        src = squiggle-lang-yarnPackage + "/libexec/@quri/squiggle-lang/";
+        buildInputs = commonBuildInputs;
+        buildPhase = ''
+          yarn --offline --check-files --cwd deps/@quri/squiggle-lang prepack
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r $src/libexec/@quri/squiggle-lang/deps/@quri/squiggle-lang/dist $out/dist
+          cp -r $src/libexec/@quri/squiggle-lang/node_modules/ $out/node_modules
+        '';
+        # passthru to spoof that this is still a yarn package even tho it's a subsequent derivation
+        workspaceDependencies = [ ];
+        pname = "@quri/squiggle-lang";
+        packageJSON = ./package.json;
       };
 
       squiggle-components-yarnPackage = pkgs.mkYarnPackage {
@@ -80,80 +78,84 @@
       };
       squiggle-components-lint = pkgs.stdenv.mkDerivation {
         name = "squiggle-components-lint";
-        src = squiggle-components-yarnPackage + "/libexec/@quri/squiggle-components/deps/@quri/squiggle-components";
+        src = squiggle-components-yarnPackage
+          + "/libexec/@quri/squiggle-components/deps/@quri/squiggle-components";
         buildPhase = "yarn lint";
         installPhase = "mkdir -p $out";
       };
       squiggle-components = pkgs.stdenv.mkDerivation {
-            name = "squiggle-components";
-            src = squiggle-components-yarnPackage + "/libexec/@quri/squiggle-components/"; # deps/@quri/squiggle-components";
-            buildInputs = commonBuildInputs;
-            buildPhase = ''
-              cd deps/@quri/squiggle-components
-              yarn all
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp -R $src/libexec/squiggle-components/deps/squiggle-components $out
-              rm $out/bin/node_modules
-              cp -R $src/libexec/squiggle-components/node_modules/. $out/node_modules
-            '';
-          };
+        name = "squiggle-components";
+        src = squiggle-components-yarnPackage
+          + "/libexec/@quri/squiggle-components/"; # deps/@quri/squiggle-components";
+        buildInputs = commonBuildInputs;
+        buildPhase = ''
+          cd deps/@quri/squiggle-components
+          yarn all
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -R $src/libexec/squiggle-components/deps/squiggle-components $out
+          rm $out/bin/node_modules
+          cp -R $src/libexec/squiggle-components/node_modules/. $out/node_modules
+        '';
+      };
 
       squiggle-website-yarnPackage = pkgs.mkYarnPackage {
-              name = "squiggle-website_source";
-              src = ./packages/website;
-              packageJSON = ./packages/website/package.json;
-              yarnLock = ./yarn.lock;
-              workspaceDependencies = [ squiggle-components-yarnPackage ];
+        name = "squiggle-website_source";
+        src = ./packages/website;
+        packageJSON = ./packages/website/package.json;
+        yarnLock = ./yarn.lock;
+        workspaceDependencies = [ squiggle-components-yarnPackage ];
 
-              # for testing
-              yarnFlags = commonYarnFlags;
+        # for testing
+        yarnFlags = commonYarnFlags;
       };
       squiggle-website-lint = pkgs.stdenv.mkDerivation {
         name = "squiggle-website-lint";
         buildInputs = commonBuildInputs;
-        src = squiggle-website-yarnPackage + "/libexec/squiggle-website/deps/squiggle-website";
+        src = squiggle-website-yarnPackage
+          + "/libexec/squiggle-website/deps/squiggle-website";
         buildPhase = "yarn lint";
         installPhase = "mkdir -p $out";
       };
       squiggle-website = pkgs.stdenv.mkDerivation {
         name = "squiggle-website";
         buildInputs = commonBuildInputs;
-        src = squiggle-website-yarnPackage + "/libexec/squiggle-website/deps/squiggle-website";
+        src = squiggle-website-yarnPackage
+          + "/libexec/squiggle-website/deps/squiggle-website";
         buildPhase = "yarn build";
         installPhase = ''
-              mkdir -p $out
-              cp -R $ src/libexec/squiggle-website/deps/squiggle-website/build $out
-              rm $out/bin/node_modules
-              cp -R $src/libexec/squiggle-website/node_modules/. $out/node_modules
+          mkdir -p $out
+          cp -R $ src/libexec/squiggle-website/deps/squiggle-website/build $out
+          rm $out/bin/node_modules
+          cp -R $src/libexec/squiggle-website/node_modules/. $out/node_modules
         '';
       };
     in rec {
 
-
       herculesCI.onPush = {
-          squiggle-lang.outputs = {
-            squiggle-lang = squiggle-lang;
-            squiggle-lang-lint = squiggle-lang-lint;
+        squiggle-lang.outputs = {
+          squiggle-lang = squiggle-lang;
+          squiggle-lang-lint = squiggle-lang-lint;
+        };
+        squiggle-components = {
+          outputs = {
+            squiggle-components = squiggle-components;
+            squiggle-components-lint = squiggle-components-lint;
           };
-          squiggle-components = {
-            outputs = {
-              squiggle-components = squiggle-components;
-              squiggle-components-lint = squiggle-components-lint;
-            };
-            # extraInputs.lang-package = {};
-          };
+          # extraInputs.lang-package = {};
+        };
 
-          squiggle-website = {
-            outputs = {
-              squiggle-website = squiggle-website;
-              squiggle-website-lint = squiggle-website-lint;
-            };
-            # extraInputs.components-package = {};
+        squiggle-website = {
+          outputs = {
+            squiggle-website = squiggle-website;
+            squiggle-website-lint = squiggle-website-lint;
           };
+          # extraInputs.components-package = {};
+        };
       };
-      defaultPackage."${system}" = herculesCI.onPush.squiggle-components.outputs.squiggle-components-lint;
+      defaultPackage."${system}" =
+        herculesCI.onPush.squiggle-components.outputs.squiggle-components-lint;
 
-      };
+    };
 }
